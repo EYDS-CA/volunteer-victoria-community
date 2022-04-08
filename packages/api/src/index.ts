148612c7360851f1e9ad4125428988a24b6438e1
noel-fw <noel@freshworks.io>
2022-04-07 23:06:45 -0700

    feat: localstack and dynamo client

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -7,7 +7,33 @@ import PostRoutes from './routes/post.routes';
 import { DynamoMapper } from './config/dynamo';
 import { User } from './schema/User.schema';
 import { Post } from './schema/Post.schema';
-import app from './app'
+import { DynamoDB } from 'aws-sdk';
+
+const dynamoDBOptions: DynamoDB.ClientConfiguration = {
+  region: 'ca-central-1',
+};
+
+dynamoDBOptions.endpoint = 'localstack:4566';
+dynamoDBOptions.sslEnabled = false;
+dynamoDBOptions.credentials = {
+  accessKeyId: 'x',
+  secretAccessKey: 'x',
+};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+const apiBaseUrl = '/api/v1';
+const client = new DynamoDB(dynamoDBOptions);
+
+app.get('/', async (req, res) => {
+  return res.json({ 'hello': 'world' });
+});
+
+app.use(`${apiBaseUrl}/users`, UserRoutes);
+app.use(`${apiBaseUrl}/posts`, PostRoutes);
 
 app.listen(4000, async () => {
   console.log('app started');
