2d8643aa7b7b7c4e087fc3fd8b8911221ad11061
noel-fw <noel@freshworks.io>
2022-04-08 15:10:47 -0700

    feat: Initial data schema

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,9 @@ import helmet from 'helmet';
 
 import UserRoutes from './routes/user.routes';
 import PostRoutes from './routes/post.routes';
-import { DynamoClient } from './config/dynamo';
+import { DynamoMapper } from './config/dynamo';
+import { User } from './schema/User.schema';
+import { Post } from './schema/Post.schema';
 
 const app = express();
 app.use(cors());
@@ -20,8 +22,14 @@ app.get('/', async (req, res) => {
 app.use(`${apiBaseUrl}/users`, UserRoutes);
 app.use(`${apiBaseUrl}/posts`, PostRoutes);
 
-app.listen(4000, () => {
+app.listen(4000, async () => {
   console.log('app started');
+  const capacity = {
+    readCapacityUnits: 5,
+    writeCapacityUnits: 5,
+  }
+  await DynamoMapper.ensureTableExists(User, capacity);
+  await DynamoMapper.ensureTableExists(Post, capacity);
 });
 
 export default app;
--- packages/api/src/types/User.ts
+++ packages/api/src/types/User.ts
@@ -1,3 +1,23 @@
+import { UserTypeEnum } from './enum/UserType';
+
 export class User {
   id: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  phoneNumber: string;
+
+  email: string;
+}
+
+export class FullUser extends User {
+  createdAt: Date;
+
+  facebookId: string;
+  
+  userType: UserTypeEnum;
+
+  recentCRC: boolean;
 }
