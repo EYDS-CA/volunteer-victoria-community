a3a617bd58c14e864c25f4d0fa518b78d71f5753
David Sabadash <DRsabadash@gmail.com>
2022-04-07 19:28:02 -0700

    implemented facebook login for 2fa

--- packages/front_end/app/src/Views/Authentication.tsx
+++ packages/front_end/app/src/Views/Authentication.tsx
@@ -1,16 +1,24 @@
 import { Button } from '@mui/material';
 import React from 'react';
 import store from 'store'
+import FacebookLogin, { ReactFacebookLoginInfo } from 'react-facebook-login';
 
 
 export default function Authentication() {
 
-  const testAuth = () => {
-    store.set('auth', {authed: true})
+  const handleCallback = (response: ReactFacebookLoginInfo) => {
+    console.log(response)
+    // store.set('auth', {authed: true})
   }
   return (
-    <div >hello
-      <Button variant="contained" onClick={testAuth}>Test auth</Button>
+    <div >
+      <FacebookLogin
+        appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+        autoLoad={true}
+        fields="name,email,picture"
+        icon="fa-facebook"
+        callback={handleCallback} 
+      />
     </div>
   )
 }
