d13f02cd0d6ba5ac30f7389b5325de2bc27b32d3
noel-fw <noel@freshworks.io>
2022-04-07 23:06:45 -0700

    feat: localstack and dynamo client

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,31 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import { DynamoDB } from 'aws-sdk';
+
+const dynamoDBOptions: DynamoDB.ClientConfiguration = {
+  region: 'ca-central-1',
+};
+
+dynamoDBOptions.endpoint = 'localstack:4566';
+dynamoDBOptions.sslEnabled = false;
+dynamoDBOptions.credentials = {
+  accessKeyId: 'x',
+  secretAccessKey: 'x',
+};
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+const client = new DynamoDB(dynamoDBOptions);
 
 app.get('/', async (req, res) => {
   return res.json({ 'hello': 'world' });
 });
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('app started');
 });
