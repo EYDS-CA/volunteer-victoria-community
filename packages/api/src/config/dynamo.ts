8333b3dce34a8a634fe436b29ec6af3d07d0e837
Amar <amarnath@freshworks.io>
2022-04-08 14:36:54 -0400

    dynamo db settings

--- packages/api/src/config/dynamo.ts
+++ packages/api/src/config/dynamo.ts
@@ -1,14 +1,20 @@
 import { DynamoDB } from 'aws-sdk';
 
 const dynamoDBOptions: DynamoDB.ClientConfiguration = {
-  region: 'ca-central-1',
+  region: process.env.AWS_DEFAULT_REGION || 'ca-central-1',
 };
 
-dynamoDBOptions.endpoint = 'localstack:4566';
+if (process.env.AWS_ACCESS_KEY_ID) {
+  dynamoDBOptions.credentials['accessKeyId'] = process.env.AWS_ACCESS_KEY_ID;
+}
+if (process.env.AWS_SECRET_ACCESS_KEY) {
+  dynamoDBOptions.credentials['secretAccessKey'] = process.env.AWS_SECRET_ACCESS_KEY;
+}
+if (process.env.AWS_SESSION_TOKEN) {
+  dynamoDBOptions.credentials['sessionToken'] = process.env.AWS_SESSION_TOKEN;
+}
+
+dynamoDBOptions.endpoint = process.env.DB_ENDPOINT;
 dynamoDBOptions.sslEnabled = false;
-dynamoDBOptions.credentials = {
-  accessKeyId: 'x',
-  secretAccessKey: 'x',
-};
 
 export const DynamoClient = new DynamoDB(dynamoDBOptions);
