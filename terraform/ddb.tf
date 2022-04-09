7675e83de72c398acfb70fe24b623cebeecf8831
Will S <will@freshworks.io>
2022-04-09 11:49:37 -0700

    Fix

--- terraform/ddb.tf
+++ terraform/ddb.tf
@@ -3,7 +3,6 @@ resource "aws_dynamodb_table" "users" {
   name           = local.users_table
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
-  range_key      = "createdAt"
 
   attribute {
     name = "id"
@@ -15,7 +14,6 @@ resource "aws_dynamodb_table" "posts" {
   name           = local.posts_table
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
-  range_key      = "createdAt"
 
   attribute {
     name = "id"
